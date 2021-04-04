722e7047b7dbf685f09a31c4b3767182a835c16f
Payton Swick <payton@foolord.com>
2021-04-04 18:00:15 -0400

    Move Field component to wpcom-checkout

--- packages/wpcom-checkout/src/index.ts
+++ packages/wpcom-checkout/src/index.ts
@@ -2,7 +2,9 @@
  * Internal dependencies
  */
 import useDisplayCartMessages from './use-display-cart-messages';
+import Field from './field';
 
 export * from './transformations';
 export { useDisplayCartMessages };
 export { createApplePayMethod } from './payment-methods/apple-pay';
+export { Field };
