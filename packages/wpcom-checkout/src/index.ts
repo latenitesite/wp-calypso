ec3ba65e1b2e0ae123fa090eb36caa5f8e9b0296
Payton Swick <payton@foolord.com>
2021-04-05 12:24:52 -0400

    Add createBancontactMethod to wpcom-checkout exports

--- packages/wpcom-checkout/src/index.ts
+++ packages/wpcom-checkout/src/index.ts
@@ -8,5 +8,6 @@ import styled from './styled';
 export * from './transformations';
 export { useDisplayCartMessages };
 export { createApplePayMethod } from './payment-methods/apple-pay';
+export * from './payment-methods/bancontact';
 export { Field };
 export { styled };
